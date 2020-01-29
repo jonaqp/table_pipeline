2f20420d9dc70002bc7a1d337d4138f15647f054
kot.yuhara <kot.yuhara@trifort.jp>
2020-01-29 12:59:43 +0900

    trainer update

--- tb_pipe/validation/cross_validate.py
+++ tb_pipe/validation/cross_validate.py
@@ -1,10 +1,42 @@
+from typing import Optional, List
 from typing import Union
 
 import numpy as np
 import pandas as pd
+from catboost import CatBoost, Pool, CatBoostClassifier
+from lightgbm import LGBMModel, LGBMClassifier
 from sklearn.model_selection import BaseCrossValidator
 
 TYPE_DATASET = Union[pd.DataFrame, np.ndarray]
+
+
+class Trainer:
+
+    def __init__(self, model: Union[LGBMModel, CatBoost]):
+        self.model = model
+        self.is_catboost = isinstance(model, CatBoost)
+        self.is_classifier = None
+
+    def train(self, train: TYPE_DATASET, target: TYPE_DATASET,
+              cat_features: Optional[List[str]] = None):
+        if self.is_catboost:
+            trn_data = Pool(train, label=target, cat_features=cat_features)
+            self.model.fit(X=trn_data)
+        else:
+            self.model.fit(X=train, y=target)
+
+    def _get_best_iteration(self):
+        if self.is_catboost:
+            return self.model.get_best_iteration()
+        else:
+            return self.model.best_iteration_
+
+    def predict(self, test: TYPE_DATASET):
+        if self.is_catboost:
+            return self.model.predict(test)
+        else:
+            return self.model.predict(test,
+                                      num_iteration=self._get_best_iteration())
 
 
 class CrossValidator:
