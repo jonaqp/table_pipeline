02049b4e646ad6b01885467b137812ed3d42aafe
kot.yuhara <kot.yuhara@trifort.jp>
2020-01-30 18:55:11 +0900

    test predict_proba

--- tests/validation/test_cross_validate.py
+++ tests/validation/test_cross_validate.py
@@ -20,3 +20,23 @@ def test_trainer_classification():
     predicts = trainer.predict(x)
     score = accuracy_score(y, predicts)
     assert score >= 0.9
+
+
+def test_trainer_predict_proba():
+    import numpy as np
+    from sklearn.datasets import load_wine
+    x, y = load_wine(True)
+
+    trainer = Trainer(LGBMClassifier())
+    trainer.train(train=x, target=y)
+    predicts = trainer.predict_proba(x)
+    predicts = np.argmax(predicts, axis=1)
+    score = accuracy_score(y, predicts)
+    assert score >= 0.9
+
+    trainer = Trainer(CatBoostClassifier(verbose=0, save_snapshot=False))
+    trainer.train(train=x, target=y)
+    predicts = trainer.predict_proba(x)
+    predicts = np.argmax(predicts, axis=1)
+    score = accuracy_score(y, predicts)
+    assert score >= 0.9
